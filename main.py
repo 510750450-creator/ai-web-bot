4b669b19f2dc683e3fe2e9bfbd8407a6270d9822
510750450-creator <510750450@qq.com>
2026-07-07 21:47:15 +0800

    add search result links

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from playwright.sync_api import sync_playwright
 keyword = input("请输入搜索内容：")
 
 with sync_playwright() as p:
+
     browser = p.chromium.launch(headless=False)
 
     page = browser.new_page()
@@ -17,16 +18,41 @@ with sync_playwright() as p:
 
     page.wait_for_timeout(5000)
 
-    # 获取搜索结果标题
-    titles = page.locator("li.b_algo h2").all_inner_texts()
+
+    # 获取搜索结果
+    results = page.locator("li.b_algo")
+
+    data = []
+
+    for r in results.all()[:5]:
+
+        try:
+            title = r.locator("h2").inner_text()
+            link = r.locator("a").first.get_attribute("href")
+
+            if title:
+                data.append({
+                    "title": title,
+                    "link": link
+                })
+
+        except:
+            pass
+
 
     print("\n搜索结果：")
-    for i, title in enumerate(titles[:5], 1):
-        print(f"{i}. {title}")
 
-    # 保存结果
+    for i, item in enumerate(data, 1):
+        print(f"{i}. {item['title']}")
+        print(item["link"])
+        print()
+
+
     with open("result.txt", "w", encoding="utf-8") as f:
-        for i, title in enumerate(titles[:5], 1):
-            f.write(f"{i}. {title}\n")
+
+        for i, item in enumerate(data, 1):
+            f.write(f"{i}. {item['title']}\n")
+            f.write(f"{item['link']}\n\n")
+
 
     browser.close()
