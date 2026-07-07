e80e915fe67a5c10cad1bc14829e5fd6fc3919a8
510750450-creator <510750450@qq.com>
2026-07-07 21:32:11 +0800

    add user input

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from playwright.sync_api import sync_playwright
 
+keyword = input("请输入搜索内容：")
+
 with sync_playwright() as p:
     browser = p.chromium.launch(headless=False)
 
@@ -9,7 +11,7 @@ with sync_playwright() as p:
 
     page.wait_for_timeout(3000)
 
-    page.locator('input[name="q"]').fill("AI 自动化赚钱")
+    page.locator('input[name="q"]').fill(keyword)
 
     page.keyboard.press("Enter")
 
